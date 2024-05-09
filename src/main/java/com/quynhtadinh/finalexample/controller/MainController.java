60e346567dfd31e50b9a25829d6bb8e4e35f41bd
quynhTD <quynhtd@oneempower.com>
2024-05-09 15:30:59 +0700

    Add new user interface

--- src/main/java/com/quynhtadinh/finalexample/controller/MainController.java
+++ src/main/java/com/quynhtadinh/finalexample/controller/MainController.java
@@ -12,38 +12,4 @@ public class MainController {
 		System.out.println("name dashboard");
 		return "dashboard";
 	}
-	
-	@RequestMapping(value = "/index", method=RequestMethod.GET)
-	public String index() {
-		System.out.println("name index");
-		return "index";
-	}
-	//layout
-	@RequestMapping(value = "/boxed", method=RequestMethod.GET)
-	public String layoutBoxed() {
-		System.out.println("name boxed");
-		return "boxed";
-	}	
-
-	@RequestMapping(value = "/fixed-header-boxed", method=RequestMethod.GET)
-	public String layoutFixedheaderboxed() {
-		System.out.println("name fixed-header-boxed");
-		return "fixed-header-boxed";
-	}
-	
-	@RequestMapping(value = "/fixed-header-fixed-mini-sidebar", method=RequestMethod.GET)
-	public String fixedheaderfixedminisidebar() {
-		System.out.println("name fixed-header-fixed-mini-sidebar");
-		return "fixed-header-fixed-mini-sidebar";
-	}
-	
-	@RequestMapping(value = "/fixed-header-menu", method=RequestMethod.GET)
-	public String fixedheadermenu() {
-		System.out.println("name fixed-header-menu");
-		return "fixed-header-menu";
-	}
-	
-
-	
-		
 }
